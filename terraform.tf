ab721ae12f8213d5c5eb6692d8622cb9dc4fd166
James Dansie <james@Jamess-MacBook-Pro.local>
2024-10-26 12:01:43 -0400

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,13 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "pip_and_louie"
 
-    workspaces {
-      name = "learn-terraform"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform"
+  #   }
+  # }
 
   required_providers {
     aws = {
